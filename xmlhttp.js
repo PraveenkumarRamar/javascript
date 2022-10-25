6dfc725e2fdc8fc01cda32c5873cbeca5486a853
Praveenkumar Ramar <113400407+PraveenkumarRamar@users.noreply.github.com>
2022-10-25 21:01:45 +0530

    Add files via upload
    XMLHTTPrequest

---
+++ xmlhttp.js
@@ -0,0 +1,7 @@
+var request  = new XMLHttpRequest();
+request.open("GET","https://restcountries.com/v2/all")
+request.send();
+request.onload = function() {
+var res = JSON.parse(request.response);
+console.log(res);
+}
